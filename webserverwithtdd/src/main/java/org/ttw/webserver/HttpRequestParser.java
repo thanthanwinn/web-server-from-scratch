d1ae2964951febec89e3a02de7f0f40c5c848399
Than Than Win <thanthanwinn27@email.com>
2026-07-17 15:20:31 +0630

    http request parser with headers and body

--- webserverwithtdd/src/main/java/org/ttw/webserver/HttpRequestParser.java
+++ webserverwithtdd/src/main/java/org/ttw/webserver/HttpRequestParser.java
@@ -1,13 +1,52 @@
 package org.ttw.webserver;
 
 
+import java.util.Objects;
+
 public class HttpRequestParser {
+
+    public static final String HEADER_KEY_VALUE_SEPARATOR = ": ";
 
     public HttpRequest parse(String rawRequest){
         HttpRequest request = new HttpRequest();
 
         String[] lines = rawRequest.split("\r\n");
         String startLine = lines[0];
+
+        parseStatusLine(request,startLine);
+
+        parseHeadersAndBody(request,lines);
+
+
+        return request;
+
+    }
+
+    private void parseHeadersAndBody(HttpRequest request, String[] lines){
+        String line;
+        int i ;
+        for( i = 1; i< lines.length; i++){
+            line = lines[i];
+
+            if(line.isEmpty()){
+                    i = i + 1;
+                    break;
+            }
+
+            String headerData[] = line.split(HEADER_KEY_VALUE_SEPARATOR);
+            String key = headerData[0];
+            String value = headerData[1];
+
+            request.setHeader(key,value);
+        }
+        if (i < lines.length){
+            String body = lines[i];
+            request.setBody(body);
+        }
+
+    }
+
+    private void parseStatusLine(HttpRequest request,String startLine){
         String startLineData[] = startLine.split(" ");
 
         String httpMethod = startLineData[0];
@@ -17,9 +56,6 @@ public class HttpRequestParser {
         request.setHttpMethod(httpMethod);
         request.setUrl(httpUrl);
         request.setHttpVersion(httpVersion);
-
-        return request;
-
     }
 }
 
--- webserverwithtdd/src/test/java/webserver/HttpRequestParserTest.java
+++ webserverwithtdd/src/test/java/webserver/HttpRequestParserTest.java
@@ -27,4 +27,47 @@ public class HttpRequestParserTest {
         Assert.assertEquals("HTTP/1.1",request.getHttpVersion());
 
     }
+    @Test
+    public void testStartLinePostWithHeader(){
+        HttpRequestParser parser = new HttpRequestParser();
+
+        String httpRequest = "POST /hello HTTP/1.1\r\n"
+                + "Content-Length: 354\r\n"
+                + "Content-Type: application/json\r\n";
+
+        HttpRequest request = parser.parse(httpRequest);
+
+        Assert.assertEquals("POST", request.getHttpMethod());
+        Assert.assertEquals("/hello", request.getUrl());
+        Assert.assertEquals("HTTP/1.1",request.getHttpVersion());
+        Assert.assertEquals("application/json",request.getHeader("Content-Type"));
+        Assert.assertEquals("354",request.getHeader("Content-Length"));
+
+    }
+
+    @Test
+    public void testStartLinePostWithBody(){
+        HttpRequestParser parser = new HttpRequestParser();
+        String body = "{data:123}";
+        String httpRequest = "POST /hello HTTP/1.1\r\n"
+                + "Content-Length: 354\r\n"
+                + "Content-Type: application/json\r\n"
+                + "\r\n"
+                +body+ "\r\n"
+                ;
+
+        HttpRequest request = parser.parse(httpRequest);
+
+        Assert.assertEquals("POST", request.getHttpMethod());
+        Assert.assertEquals("/hello", request.getUrl());
+        Assert.assertEquals("HTTP/1.1",request.getHttpVersion());
+        Assert.assertEquals("application/json",request.getHeader("Content-Type"));
+        Assert.assertEquals("354",request.getHeader("Content-Length"));
+        Assert.assertEquals(body,request.getBody());
+
+
+
+
+
+    }
 }
